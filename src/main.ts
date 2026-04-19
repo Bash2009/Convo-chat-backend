4ac86653409a4951c36f4d441fcb93962dcb0f7d
Bash2009 <athekamebash@gmail.com>
2026-04-19 21:19:39 +0100

    Fixing cors issue

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [process.env.FRONTEND_URL ?? 'http://localhost:5173'],
+    origin: [process.env.FRONTEND_URL || 'http://localhost:5173'],
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
     credentials: true,
   });
 
   app.use((req, res, next) => {
     if (req.method === 'OPTIONS') {
-      res.setHeader('Access-Control-Allow-Origin', [process.env.FRONTEND_URL ?? 'http://localhost:5173']);
+      res.setHeader('Access-Control-Allow-Origin', [
+        process.env.FRONTEND_URL || 'http://localhost:5173',
+      ]);
 
       res.setHeader(
         'Access-Control-Allow-Methods',
