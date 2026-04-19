53b33f9abca3b0b944bd2b4de52e86a9d1b88062
Bash2009 <athekamebash@gmail.com>
2026-04-19 23:18:25 +0100

    fixing cors issue in websocket

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -16,7 +16,7 @@ import { CreateChatDto } from './dto/create-chat.dto';
 
 @WebSocketGateway({
   cors: {
-    origin: process.env.CLIENT_URL ?? 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL ?? 'http://localhost:5173',
     credentials: true,
   },
 })
